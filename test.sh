60e6b2658f99259909e2c748fc2011a2bd38c157
John Warren <jhewarren@gmail.com>
2018-02-09 22:43:38 -0800

    net config test

--- test.sh
+++ test.sh
@@ -8,7 +8,17 @@ innet="$_AB.10"
 _ws="5"
 exip="$exnet.$_ws"
 inip="$innet.$_ws"
-echo
-echo "EX: $exif $exip $exnet.0/24"
-echo "IN: $inif $inip $innet.0/24"
-echo
+
+function config_net(){
+    echo
+    echo "EX: $exif $exip $exnet.0/24"
+    echo "IN: $inif $inip $innet.0/24"
+    echo
+    echo "ip addr add $inip dev $inif"
+    echo "ip link set $inif up"
+#    echo "1" > /proc/sys/net/ipv4/ip_forward
+    echo "ip route add $innet.0/24 via $inip dev $inif"
+    echo "ip route add $exnet.0/24 via $exip dev $exif"
+}
+
+config_net

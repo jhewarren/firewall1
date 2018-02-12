8775e17f2b46c952460b01d400be348ff8ecd2ce
Peyman Tehrani Parsa <peymantparsa@gmail.com>
2018-02-12 01:02:04 -0800

    made test script for extetnal attacks

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
+echo 'Enter IP'
+read ip
+test="hping3 $ip -V"
 
-exif="en0"
-inif="enp0s8"
-_AB="192.168"
-exnet="$_AB.0"
-innet="$_AB.10"
-_ws="5"
-exip="$exnet.$_ws"
-inip="$innet.$_ws"
+#SYN FIN test
+$test -c 1 -s 1000 -p 80 -SF &
 
-function config_net(){
-    echo
-    echo "EX: $exif $exip $exnet.0/24"
-    echo "IN: $inif $inip $innet.0/24"
-    echo
-    echo "ip addr add $inip dev $inif"
-    echo "ip link set $inif up"
-#    echo "1" > /proc/sys/net/ipv4/ip_forward
-    echo "ip route add $innet.0/24 via $inip dev $inif"
-    echo "ip route add $exnet.0/24 via $exip dev $exif"
-}
+#Telnet
+$test -c 1 -S -s 80 -p 23 &
 
-config_net
+#Block all external traffic directed to TCP ports 32768 – 32775, 137 – 139
+$test -c 7 -S -p 32768-32775 -s 80 &
+$test -c 2 -S -p 137-139 -s 80 &
+
+##Block all external traffic directed to UDP ports 32768 – 32775, 137 – 139
+$test -c 7 --udp -S -p 32768-32775 -s 80 &
+$test -c 2 --udp -S -p 137-139 -s 80 &
+
+##Block all external traffic directed to TCP ports 111 and 515.
+$test -c 1 -S -p 111 -s 80 &
+$test -c 1 -S -p 515 -s 80 &
+
+#Spoof subnet
+$test -c 1 -S -s 80 -p 80 --spoof 192.168.10.5
